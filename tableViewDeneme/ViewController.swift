9ae543054692e3299bd1dcb1ba2c2b15f569cb0f
nzm.avci@gmail.com <nzm.avci@gmail.com>
2017-05-20 00:45:30 +0300

    denim

--- tableViewDeneme/ViewController.swift
+++ tableViewDeneme/ViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
+    
+    
+    var users = ["nazim","deniz","azize","ela","bahar"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return users.count
+    }
+    
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        
+        cell.textLabel?.text = users[indexPath.row]
+        
+        return cell
+        // asdasdkjasdjaŞDJKadşlajksdşskaj
+    }
 
 }
 
